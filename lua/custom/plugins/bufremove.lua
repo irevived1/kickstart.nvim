681ef669763162326f141a982370b5da2b719e93
lsanto <lisbethsanto64@gmail.com>
2024-09-30 00:18:01 -0400

    add alt+q for buff remove

--- lua/custom/plugins/bufremove.lua
+++ lua/custom/plugins/bufremove.lua
@@ -27,6 +27,11 @@ return {
         miniDeleteBuffer,
         desc = 'Delete Buffer',
       },
+      {
+        '<M-q>',
+        miniDeleteBuffer,
+        desc = 'Delete Buffer',
+      },
     -- stylua: ignore
     { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
     },
