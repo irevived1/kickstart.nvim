b4b630245af12195a6d7f7ce3dcd6b1d5badf7e4
lsanto <lisbethsanto64@gmail.com>
2024-10-05 23:56:39 -0400

    add eslint top mason

--- lua/custom/plugins/formatting.lua
+++ lua/custom/plugins/formatting.lua
@@ -1,30 +1,30 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  'stevearc/conform.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
   config = function()
-    local conform = require("conform")
+    local conform = require 'conform'
 
-    conform.setup({
+    conform.setup {
       formatters_by_ft = {
-        javascript = { "eslint_d" },
-        typescript = { "eslint_d" },
-        javascriptreact = { "eslint_d" },
-        typescriptreact = { "eslint_d" },
-        svelte = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        graphql = { "prettier" },
-        liquid = { "prettier" },
-        lua = { "stylua" },
+        javascript = { 'eslint_d' },
+        typescript = { 'eslint_d' },
+        javascriptreact = { 'eslint_d' },
+        typescriptreact = { 'eslint_d' },
+        svelte = { 'prettier' },
+        css = { 'prettier' },
+        html = { 'prettier' },
+        json = { 'prettier' },
+        yaml = { 'prettier' },
+        markdown = { 'prettier' },
+        graphql = { 'prettier' },
+        liquid = { 'prettier' },
+        lua = { 'stylua' },
       },
       format_on_save = {
         lsp_fallback = true,
         async = false,
         timeout_ms = 1000,
       },
-    })
+    }
   end,
 }
