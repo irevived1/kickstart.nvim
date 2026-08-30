25ccb75ff7aeecc0633a9a8c8eebfaee5cf174ee
Ahmet Calis <calisahmet@gmail.com>
2026-08-30 11:48:44 -0700

    fix(treesitter): update to nvim-treesitter v1.x API

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -5,27 +5,18 @@ return {
     build = ':TSUpdate',
     dependencies = {
       'windwp/nvim-ts-autotag',
+      'nvim-treesitter/nvim-treesitter-textobjects',
     },
     config = function()
-      -- import nvim-treesitter plugin
-      local treesitter = require 'nvim-treesitter.configs'
-
-      -- configure treesitter
-      treesitter.setup { -- enable syntax highlighting
-        modules = {},
-        ignore_install = {},
-        sync_install = false,
+      require('nvim-treesitter').setup {
         auto_install = true,
         highlight = {
           enable = true,
         },
-        -- enable indentation
         indent = { enable = true },
-        -- enable autotagging (w/ nvim-ts-autotag plugin)
         autotag = {
           enable = true,
         },
-        -- ensure these language parsers are installed
         ensure_installed = {
           'json',
           'javascript',
@@ -59,25 +50,18 @@ return {
         textobjects = {
           select = {
             enable = true,
-
-            -- Automatically jump forward to textobj, similar to targets.vim
             lookahead = true,
-
             keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
               ['af'] = '@function.outer',
               ['if'] = '@function.inner',
               ['ac'] = '@class.outer',
-              -- You can optionally set descriptions to the mappings (used in the desc parameter of
-              -- nvim_buf_set_keymap) which plugins like which-key display
               ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
-              -- You can also use captures from other query groups like `locals.scm`
               ['as'] = { query = '@scope', query_group = 'locals', desc = 'Select language scope' },
             },
             selection_modes = {
-              ['@parameter.outer'] = 'v', -- charwise
-              ['@function.outer'] = 'V', -- linewise
-              ['@class.outer'] = '<c-v>', -- blockwise
+              ['@parameter.outer'] = 'v',
+              ['@function.outer'] = 'V',
+              ['@class.outer'] = '<c-v>',
             },
             include_surrounding_whitespace = true,
           },
