9d111a4a076e55046f5797f1c2000701d0e7ab79
Ahmet Calis <acalis@turo.com>
2026-08-30 16:22:23 -0700

    fix(myconfigs): double-schedule focus restore to beat Snacks internal schedule

--- lua/myconfigs.lua
+++ lua/myconfigs.lua
@@ -101,12 +101,14 @@ vim.api.nvim_create_autocmd('User', {
   callback = function()
     local win = vim.api.nvim_get_current_win()
     Snacks.explorer()
-    -- defer so Snacks finishes moving focus to the explorer before we move it back
-    vim.defer_fn(function()
-      if vim.api.nvim_win_is_valid(win) then
-        vim.api.nvim_set_current_win(win)
-      end
-    end, 100)
+    -- double-schedule so we run after Snacks' own internal vim.schedule focus grab
+    vim.schedule(function()
+      vim.schedule(function()
+        if vim.api.nvim_win_is_valid(win) then
+          vim.api.nvim_set_current_win(win)
+        end
+      end)
+    end)
   end,
 })
 
